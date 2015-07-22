aef27cf1f9af8171106666cbaf011f4e8a3a6b03
xiangshouding <fansekey@gmail.com>
2015-07-22 02:47:03 +0000

    based on https://github.com/fis-components/components/blob/master/modules/jquery.js

--- jquery.js
+++ jquery.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery JavaScript Library v2.1.3
+ * jQuery JavaScript Library v2.1.4
  * http://jquery.com/
  *
  * Includes Sizzle.js
@@ -9,7 +9,7 @@
  * Released under the MIT license
  * http://jquery.org/license
  *
- * Date: 2014-12-18T15:11Z
+ * Date: 2015-04-28T16:01Z
  */
 
 (function (global, factory) {
@@ -47,7 +47,7 @@
     var support = {};
     var
         // Use the correct document accordingly with window argument (sandbox)
-        document = window.document, version = '2.1.3',
+        document = window.document, version = '2.1.4',
         // Define a local copy of jQuery
         jQuery = function (selector, context) {
             // The jQuery object is actually just the init constructor 'enhanced'
@@ -387,7 +387,11 @@
         class2type['[object ' + name + ']'] = name.toLowerCase();
     });
     function isArraylike(obj) {
-        var length = obj.length, type = jQuery.type(obj);
+        // Support: iOS 8.2 (not reproducible in simulator)
+        // `in` check used to prevent JIT error (gh-2145)
+        // hasOwn isn't used here due to false negatives
+        // regarding Nodelist length in IE
+        var length = 'length' in obj && obj.length, type = jQuery.type(obj);
         if (type === 'function' || jQuery.isWindow(obj)) {
             return false;
         }
